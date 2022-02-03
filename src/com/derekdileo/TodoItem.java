0a9ca8a0282f7d98795e9d13a29852bfc4646a00
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-02-03 10:47:40 -0500

    To enable class persistence with Hibernate, add private field "id" to TodoItem class along with a default constructor. Create getters and setters for each field.

--- src/com/derekdileo/TodoItem.java
+++ src/com/derekdileo/TodoItem.java
@@ -1,17 +1,31 @@
 package com.derekdileo;
 
 public class TodoItem {
-    // Instance variable(s)
+    // Instance variables (id is required for Hibernate / DB)
+    private int id;
     private String description;
 
-    // Constructor
+    // Default Constructor (needed to be persistent class for Hibernate) (POJO model)
+    public TodoItem() {}
     public TodoItem(String description) {
         this.description = description;
     }
 
-    // Getter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @Override
